dd1f51f6dceda4af42565c852f30ad7b4bb8b602
Taylor Smith <smith.taylord@gmail.com>
2023-04-19 15:32:10 -0600

    search form is working

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,13 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../assets/scss/components/SearchBar.scss'
 import { spotifyService } from '../services/SpotifyService.js';
 import Pop from '../utils/Pop.js';
 
 export default function SearchBar() {
-    const getAccessToken = async (e) => {
+    const [query, setQuery] = useState('')
+
+    const searchTracks = async (e) => {
         e.preventDefault()
         try {
-            await spotifyService.searchTracks('Cake')
+            await spotifyService.searchTracks(query)
         }
         catch (error) {
             Pop.error(error);
@@ -19,15 +21,17 @@ export default function SearchBar() {
         <div className="container-fluid">
             <div className="row">
                 <div className="col-12 p-5 text-center">
-                    <form onSubmit={getAccessToken}
+                    <form onSubmit={searchTracks}
                         className='container'>
                         <div className="row">
                             <div className="col-12 col-md-6 m-auto">
                                 <input
+                                    onChange={(e) => setQuery(e.target.value)}
                                     className="form-control form-control-lg mb-5"
                                     type="text"
                                     placeholder="Search your favorite songs..."
-                                    aria-label="search songs from spotify" />
+                                    aria-label="search songs from spotify"
+                                    required />
                                 <button
                                     type='submit'
                                     className='btn bg-primary selectable border rounded-pill fs-3 px-5 text-light'>Search</button>
