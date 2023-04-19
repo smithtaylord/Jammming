41235544ccdd0c701388fe5bffde7280ab7ee6ac
Taylor Smith <smith.taylord@gmail.com>
2023-04-19 14:25:58 -0600

    Still not working nice with spotify

--- src/services/SpotifyService.js
+++ src/services/SpotifyService.js
@@ -2,28 +2,33 @@ import { AppState } from "../AppState.js"
 import { logger } from "../utils/Logger.js";
 
 class SpotifyService {
-    clientId = 'f84af62081324782afbc2cffa9f5ee05&client_secret=34cdf68cf57d4480a26a8b5a07e6c2e7'; // Insert client ID here.
-    redirectUri = 'http://localhost:8080/';
-    accessToken = ''
+    clientId = 'f84af62081324782afbc2cffa9f5ee05'; // Insert client ID here.
+    redirectUri = 'http://localhost:8080/spotify-callback/';
+    authEndpoint = 'https://accounts.spotify.com/authorize'
+    responseType = 'token'
+    // accessToken = ''
 
     async getToken() {
-        if (this.accessToken) {
-            return this.accessToken;
-        }
+        // if (this.accessToken) {
+        //     return this.accessToken;
+        // }
 
-        const accessTokenMatch = window.location.href.match(/access_token=([^&]*)/);
-        const expiresInMatch = window.location.href.match(/expires_in=([^&]*)/);
-        if (accessTokenMatch && expiresInMatch) {
-            this.accessToken = accessTokenMatch[1];
-            const expiresIn = Number(expiresInMatch[1]);
-            window.setTimeout(() => this.accessToken = '', expiresIn * 1000);
-            window.history.pushState('Access Token', null, '/'); // This clears the parameters, allowing us to grab a new access token when it expires.
-            return this.accessToken;
-        }
+        // const accessTokenMatch = window.location.href.match(/access_token=([^&]*)/);
+        // const expiresInMatch = window.location.href.match(/expires_in=([^&]*)/);
+        // if (accessTokenMatch && expiresInMatch) {
+        //     this.accessToken = accessTokenMatch[1];
+        //     const expiresIn = Number(expiresInMatch[1]);
+        //     window.setTimeout(() => this.accessToken = '', expiresIn * 1000);
+        //     window.history.pushState('Access Token', null, '/'); // This clears the parameters, allowing us to grab a new access token when it expires.
+        //     return this.accessToken;
+        // }
 
-        const accessUrl = `https://accounts.spotify.com/authorize?client_id=${this.clientId}&response_type=token&scope=playlist-modify-public&redirect_uri=${this.redirectUri}`;
-        window.location = accessUrl
-        logger.log(accessUrl)
+        // const accessUrl = `https://accounts.spotify.com/authorize?client_id=${this.clientId}&response_type=token&scope=playlist-modify-public&redirect_uri=${this.redirectUri}`;
+        // window.open = accessUrl
+        // // window.location = 'https://google.com'
+        // logger.log(accessUrl)
+
+        window.location = `${this.authEndpoint}?client_id=${this.clientId}&redirect_uri=${this.redirectUri}&response_type=${this.responseType}`
     }
 
 }
