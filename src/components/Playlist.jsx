012fada116f2a939d20c9cfbeb6e30cec5e4378d
Taylor Smith <smith.taylord@gmail.com>
2023-04-20 16:55:36 -0600

    Can name your playlist

--- src/components/Playlist.jsx
+++ src/components/Playlist.jsx
@@ -1,24 +1,43 @@
-import React from 'react';
+/* eslint-disable react/prop-types */
+import React, { useState } from 'react';
 import '../assets/scss/components/Playlist.scss'
-import Track from './Track.jsx';
 import Tracklist from './Tracklist.jsx';
-import { AppState } from '../AppState.js';
-import { observer } from 'mobx-react';
+import Pop from '../utils/Pop.js';
+import { logger } from '../utils/Logger.js';
+
 
 export default function Playlist({ tracks }) {
-    const playlist = AppState.playlist
+    const [playlistName, setPlaylistName] = useState('')
+
+    const createPlaylist = async () => {
+        try {
+            logger.log(playlistName, '[playlist name]')
+        }
+        catch (error) {
+            Pop.error(error);
+        }
+    }
 
     return (
 
         <div className="Playlist main-bg rounded elevation-5">
             <div className='p-3'>
-                <h1 className='text-light text-shadow mb-3 px-3'>Playlist</h1>
+                {/* <h1 className='text-light text-shadow mb-3 px-3'>Playlist</h1> */}
+                <div className='text-center'>
+                    <input
+                        className='text-info text-shadow mb-3 px-3 main-bg fs-3 text-center form-control'
+                        onChange={(e) => setPlaylistName(e.target.value)}
+                        type="text"
+                        placeholder='playlist name...' />
+                </div>
                 {
                     tracks.length > 0 &&
                     <Tracklist tracks={tracks} />
                 }
                 <div className='text-center mt-4 mb-2'>
-                    <button className='btn bg-primary selectable border rounded-pill fs-3 px-5 text-light'>
+                    <button
+                        onClick={createPlaylist}
+                        className='btn bg-primary selectable border rounded-pill fs-3 px-5 text-light'>
                         Add to Spotify</button>
                 </div>
             </div>
