7047012d2a767ffb045a00d83eafbeb2a9f46777
Taylor Smith <smith.taylord@gmail.com>
2023-04-21 16:25:21 -0600

    adding an icon back in to test something

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
-// import mdiPlaylistMusic from '@mdi/js';
-// import Icon from '@mdi/react'
+import Icon from '@mdi/react';
+import { mdiPlaylistMusic } from '@mdi/js';
 import { AppState } from "../AppState.js";
 import { observer } from 'mobx-react-lite';
 import Playlist from "./Playlist.jsx";
@@ -27,7 +27,10 @@ function Navbar() {
           <div>
             <div className="dropdown">
               <button className="btn bg-primary position-relative" type="button" data-bs-toggle="dropdown" aria-expanded="false">
-                <span className="me-2">@&@</span>
+                <span className="me-2">
+                  <Icon path={mdiPlaylistMusic} size={1} />
+
+                </span>
                 <span className="position-absolute top-0 end-0 pe-1 fw-bold text-info">{AppState.playlist.length}</span>
               </button>
               <ul className="dropdown-menu main-bg XL">
