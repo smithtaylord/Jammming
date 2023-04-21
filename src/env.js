36afa88c8e977f21788d759d5df051591baf6448
Taylor Smith <smith.taylord@gmail.com>
2023-04-21 11:40:00 -0600

    trying to build again

--- src/env.js
+++ src/env.js
@@ -1,5 +1,5 @@
 export const dev = window.location.origin.includes('localhost')
-export const baseURL = dev ? 'http://localhost:3000' : ''
+export const baseURL = dev ? 'http://localhost:8080' : ''
 export const useSockets = false
 export const domain = ''
 export const clientId = ''
